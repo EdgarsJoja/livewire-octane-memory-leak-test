e14218eb3447074484ed4aedae3e8ed1e2017010
Edgars Joja <ejoja@mikrotik.com>
2025-01-27 14:49:15 +0200

    Setup initial page

--- resources/views/components/livewire-page.blade.php
+++ resources/views/components/livewire-page.blade.php
@@ -2,19 +2,22 @@
     Timestamp: {{ time() }}
     <br />
     Used memory: {{ get_memory() }}
+    <br />
 
     @if($type === 'laravel')
         <x-laravel-memory-test />
     @elseif($type === 'livewire')
         <livewire:livewire-memory-test />
     @else
-        Type: {{ $type }}
-        path must be `/memory-test/laravel` or `/memory-test/livewire`
+        Type: {{ $type ?: 'UNKNOWN' }}.
+        Path must be `/memory-test/laravel` or `/memory-test/livewire`
     @endif
 
     <style>
         html, body {
             height: 100%;
+            background: #181C14;
+            color: #ECDFCC;
         }
 
         .container {
