c439fc27f4f2bb8083da6d8b0c0f73b1bfc1ea0a
Edgars Joja <ejoja@mikrotik.com>
2025-06-02 09:25:54 +0300

    Re-install vendor each run

--- run.sh
+++ run.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
 
-docker run --rm -it -v "$PWD":/app --user "$(id -u)":"$(id -g)" composer:latest install
+if ! [ -d ./vendor ]; then
+  docker run --rm -it -v "$PWD":/app --user "$(id -u)":"$(id -g)" composer:latest install
+fi
+
 ./frankenphp php-cli artisan octane:frankenphp
